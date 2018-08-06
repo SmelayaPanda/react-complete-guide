6b4628b6e13548aea0ff5c687d44adc7e7a7e72d
panda <SmelayaPanda@mail.ru>
2018-08-07 00:35:31 +0700

    returning multiple root components

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "../Cockpit/Cockpit.css";
+// import Aux from './../../hoc/Aux'
 
 const cockpit = props => {
   const pStyle = []
-  let btnClass = ''
+  let btnClass = styles.Button
   if (props.showPersons) {
-    btnClass = styles.Red
+    btnClass = [styles.Red, styles.Button].join(' ')
   }
   if (props.persons.length <= 2) {
     pStyle.push(styles.red)
@@ -13,13 +14,17 @@ const cockpit = props => {
   if (props.persons.length <= 1) {
     pStyle.push(styles.bold)
   }
-
+  // How to return multiple root elements?
+  // 1. return [ <h1>...</h1>, <p>...</p>]
+  // 2. wrap into <React.fragment></React.fragment>
+  // 3. wrap into custom <Aux></Aux>
+  const Aux = props => props.children
   return (
-    <div className={styles.Cockpit}>
+    <Aux>
       <h1>Hi, I'm a react app!</h1>
       <p className={pStyle.join(' ')}>This is really works</p>
       <button className={btnClass} onClick={props.clicked}>Toggle persons</button>
-    </div>
+    </Aux>
   )
 }
 
