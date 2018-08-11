10f63c48dd6549cb32cc80f96691027ad2121cbf
panda <SmelayaPanda@mail.ru>
2018-08-11 20:54:53 +0700

    active link

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, Link} from 'react-router-dom'
+import {Route, NavLink} from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts'
 import NewPost from '../../containers/Blog/NewPost/NewPost'
@@ -11,12 +11,14 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to={'/'}>Home</Link></li>
-              <li><Link to={{
+              {/* NavLink add class active to <a>
+               You can override active classname by passing prop activeClassName={'my-active'}*/}
+              <li><NavLink exact to={'/'}>Home</NavLink></li>
+              <li><NavLink exact to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick-submit=true'
-              }}>New Post</Link></li>
+              }}>New Post</NavLink></li>
             </ul>
           </nav>
         </header>
