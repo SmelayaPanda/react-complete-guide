322a0f605ff3bd8c998653177f9ec907f1ff5fc1
panda <SmelayaPanda@mail.ru>
2018-08-11 23:01:48 +0700

    switch route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Route, NavLink} from 'react-router-dom'
+import {Route, NavLink, Switch} from 'react-router-dom'
 import './Blog.css';
 import Posts from './Posts/Posts'
 import FullPost from '../Blog/FullPost/FullPost'
@@ -24,9 +24,12 @@ class Blog extends Component {
           </nav>
         </header>
         {/*<Route path={'/'} exact render={() => <h1>Home 1</h1>} />*/}
+        {/* Switch tells react load only one route at the time - first matched */}
         <Route path={'/'} exact component={Posts} />
-        <Route path={'/new-post'} component={NewPost} />
-        <Route path={'/:id'} exact component={FullPost} />
+        <Switch>
+          <Route path={'/new-post'} component={NewPost} />
+          <Route path={'/:id'} exact component={FullPost} />
+        </Switch>
       </div>
     );
   }
