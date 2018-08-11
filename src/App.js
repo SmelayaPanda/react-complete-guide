966a17a8933fab65bbe7298dc3e4a14a873b68ba
panda <SmelayaPanda@mail.ru>
2018-08-12 00:53:55 +0700

    BrowserRouter basename

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      // You can have on the server only specific part of routes handled by react
+      // another request can be handled by server or another file in root directory
+      // <BrowserRouter basename={'/my-app'}>
+      <BrowserRouter basename={'/'}>
         <div className="App">
           <Blog/>
         </div>
