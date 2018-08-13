605fff385065380a8570f3b280e3db51728c5f54
panda <SmelayaPanda@mail.ru>
2018-08-13 20:13:04 +0700

    redux -start

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, {Component} from 'react';
-import {BrowserRouter} from 'react-router-dom'
+import React, { Component } from 'react';
 
-import Blog from './containers/Blog/Blog';
+import Counter from './containers/Counter/Counter';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      // You can have on the server only specific part of routes handled by react
-      // another request can be handled by server or another file in root directory
-      // <BrowserRouter basename={'/my-app'}>
-      <BrowserRouter basename={'/'}>
-        <div className="App">
-          <Blog/>
-        </div>
-      </BrowserRouter>
+      <div className="App">
+       <Counter />
+      </div>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,45 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import axios from 'axios'
 
-// global axios parameters
-axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
-axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN'
-axios.defaults.headers.post['Content-Type'] = 'application/json'
-
-axios.interceptors.request.use(request => {
-  console.log(request)
-  // Edit config
-  return request // you always need return request in interceptors
-  // otherwise you block any requests
-}, error => { // request sending error (as not internet connection, but not failed request result)
-  console.log(error);
-  return Promise.reject(error)
-})
-
-
-axios.interceptors.response.use(response => {
-  console.log(response)
-  // Edit config
-  return response // you always need return
-  // otherwise you block any response
-}, error => {
-  console.log(error);
-  return Promise.reject(error)
-})
-
-
-// // If you may need to remove an interceptor later you can.
-//
-// const myInterceptor = axios.interceptors.request.use(function () {/*...*/
-// });
-// axios.interceptors.request.eject(myInterceptor);
-// // You can add interceptors to a custom instance of axios.
-//
-// const instance = axios.create();
-// instance.interceptors.request.use(function () {/*...*/
-// });
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
